aa6ac627d63cbaf74ce5d9733c9bafde111bc89d
Manavparmar07 <162402988+Manavparmar07@users.noreply.github.com>
2025-04-17 11:54:37 +0530

    Create Practical 6.2.C

---
+++ Practical 6.2.C
@@ -0,0 +1,13 @@
+<?php 
+
+funcƟon removeSpaces($str) { 
+
+return preg_replace('/\s+/', '', $str); 
+
+} 
+
+$input = "he llo wo rld"; 
+
+echo "Without spaces: " . removeSpaces($input) . "<br>"; 
+
+?>
